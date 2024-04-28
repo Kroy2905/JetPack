3e3a4a6858fb6428dabff7a642d3e5682b4e81a2
koustavroy <koustav.roy@myelinfoundry.com>
2024-04-28 23:42:44 +0530

    item list view completed

--- app/src/main/java/com/kroy/composestudy/MainActivity.kt
+++ app/src/main/java/com/kroy/composestudy/MainActivity.kt
@@ -5,8 +5,13 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
@@ -22,11 +27,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kroy.composestudy.ui.theme.ComposeStudyTheme
 
@@ -34,37 +43,104 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PreviewFunction()
-//            ComposeStudyTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Greeting("Android")
-//                }
-//            }
+
+            ComposeStudyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ListViewItem(R.drawable.img,"Koustav","Android Developer")
+                }
+            }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ListViewItem(imgID:Int , name:String ,profession:String){
+    Row(horizontalArrangement = Arrangement.Start,
+        modifier = Modifier
+            .padding(10.dp)) {
+        Column (){
+            Box(
+                contentAlignment = Alignment.Center
+            ){
+                Image(painter = painterResource(id = imgID),
+                    contentDescription = "bg",
+                    Modifier.size(80.dp),
+                    contentScale = ContentScale.Inside,
+
+
+                    )
+                Image(painter = painterResource(id = R.drawable.person),
+                    contentDescription = "person",
+                    Modifier.
+                    size(40.dp)
+                    )
+            }
+        }
+        Column(
+            Modifier
+                .padding(5.dp,10.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(
+                text = name,
+                fontSize = 25.sp,
+                fontStyle =FontStyle.Normal,
+                fontWeight = FontWeight.ExtraBold,
+                maxLines = 1
+
+                )
+            Text(
+                text = profession,
+                fontSize = 18.sp,
+                fontStyle =FontStyle.Normal,
+                maxLines = 1
+
+
+
+            )
+        }
+
+    }
 }
 
-
+@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeStudyTheme {
-        Greeting("Android")
+fun Row_and_Column () {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "A",
+
+        )
+        Text(
+            text = "B"
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.Bottom
+        ){
+            Text(
+                text = "C",
+
+                )
+            Text(
+                text = "D"
+            )
+        }
     }
 
 }
-@Preview(showBackground = true)
+
+
+
+
 @Composable
 fun PreviewFunction(){
     /** Text */
